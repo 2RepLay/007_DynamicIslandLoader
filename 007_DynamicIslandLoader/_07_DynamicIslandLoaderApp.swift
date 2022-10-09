88baf9aaa09e3187748b01738aca42260c691528
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-10-09 18:49:34 +0300

    Initial Commit

---
+++ 007_DynamicIslandLoader/_07_DynamicIslandLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  _07_DynamicIslandLoaderApp.swift
+//  007_DynamicIslandLoader
+//
+//  Created by nikita on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct _07_DynamicIslandLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
