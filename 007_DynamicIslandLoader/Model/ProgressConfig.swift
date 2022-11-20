af2b70af364080e6127eaa12b60ca68f59702397
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-11-20 19:02:22 +0200

    Finished Daily

---
+++ 007_DynamicIslandLoader/Model/ProgressConfig.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressConfig.swift
+//  007_DynamicIslandLoader
+//
+//  Created by nikita on 09.10.2022.
+//
+
+import SwiftUI
+
+struct ProgressConfig {
+	
+	var title: String
+	var progressImage: String
+	var expandedImage: String
+	var tint: Color
+	var rotationEnabled: Bool = false
+	
+}
